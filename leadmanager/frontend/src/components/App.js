2a62c1190964a72f7484638d6ff5ecb2662c21cf
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-08-01 18:22:32 +0200

    Add auth state

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -14,6 +14,7 @@ import PrivateRoute from './common/PrivateRoute'
 
 import { Provider } from 'react-redux'
 import store from '../store'
+import { loadUser } from '../actions/auth'
 
 // Alert Options
 const alertOptions = {
@@ -22,6 +23,10 @@ const alertOptions = {
 }
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser())
+  }
+
   render() {
     return (
       <Provider store={store}>
