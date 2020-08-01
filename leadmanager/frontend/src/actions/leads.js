5f91e0444f5427f6f3351553535e8f06dc1b990a
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-08-01 08:11:17 +0200

    Add authentication requirement for Add Lead and Get Leads

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-import { createMessage } from './messages'
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types'
+import { createMessage, returnErrors } from './messages'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types'
 
 // Events management
 
@@ -14,7 +14,7 @@ export const getLeads = () => dispatch => { // nested function possible by using
         type: GET_LEADS,
         payload: res.data
       })
-    }).catch(err => console.log(err))
+    }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
 }
 
 // DELETE LEADS
@@ -40,14 +40,5 @@ export const addLead = (lead) => dispatch => { // nested function possible by us
         type: ADD_LEAD,
         payload: res.data
       })
-    }).catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      }
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      })
-    })
+    }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
 }
