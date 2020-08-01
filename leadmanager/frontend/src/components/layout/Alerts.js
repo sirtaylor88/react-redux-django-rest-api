209a396aab9f3a2909c2e10f8704b3312da7c8cf
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-08-01 20:41:34 +0200

    Add alert messages and welcome text

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -18,10 +18,12 @@ export class Alerts extends Component {
   componentDidUpdate(prevProps) {
     const { error, alert, message } = this.props
     if (error !== prevProps.error ) {
-      if (error.msg.name)    alert.error(`Name: ${error.msg.name.join()}`)
-      if (error.msg.email)   alert.error(`Email: ${error.msg.email.join()}`)
-      if (error.msg.message) alert.error(`Message: ${error.msg.message.join()}`)
+      if (error.msg.name)             alert.error(`Name: ${error.msg.name.join()}`)
+      if (error.msg.email)            alert.error(`Email: ${error.msg.email.join()}`)
+      if (error.msg.message)          alert.error(`Message: ${error.msg.message.join()}`)
+      if (error.msg.non_field_errors) alert.error(error.msg.non_field_errors.join())
     }
+
     if (message != prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead)
       if (message.addLead) alert.success(message.addLead)
--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -15,6 +15,11 @@ export class Header extends Component {
 
     const authLinks = (
       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        <span className="navbar-text mr-3">
+          <strong>
+            { user ? `Welcome ${user.username}` : "" }
+          </strong>
+        </span>
         <li className="nav-item">
           <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">Logout</button>
         </li>
