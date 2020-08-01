826438b5bfed0d3bb147a5e8053d2fdb1ca8af7c
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-08-01 11:48:08 +0200

    Add Register and Login Forms

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 
 import { positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
@@ -7,6 +8,8 @@ import AlertTemplate from 'react-alert-template-basic'
 import Alerts from './layout/Alerts'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+import Login from './accounts/Login'
+import Register from './accounts/Register'
 
 import { Provider } from 'react-redux'
 import store from '../store'
@@ -22,13 +25,19 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            < Header />
-            < Alerts />
-            <div className="container">
-              < Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              < Header />
+              < Alerts />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Dashboard}/>
+                  <Route exact path="/register" component={Register}/>
+                  <Route exact path="/login" component={Login}/>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     )
