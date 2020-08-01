744126ca68980049554b7e00172aa3a34d6a4c6f
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-08-01 23:35:43 +0200

    Refacto actions/auth.js

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -15,23 +15,8 @@ export const loadUser = () => (dispatch, getState) => {
   // User Loading
   dispatch({ type: USER_LOADING })
 
-  // Get token from state
-  const token = getState().auth.token
-
-  // Headers
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }
-
-  // If token, add to headers config
-  if (token) {
-    config.headers['Authorization'] = `Token ${token}`
-  }
-
   axios
-    .get('/api/auth/user', config)
+    .get('/api/auth/user', tokenConfig(getState))
     .then(res => {
       dispatch({
         type: USER_LOADED,
@@ -74,6 +59,19 @@ export const login = (username, password) => dispatch => {
 
 // LOGOUT USER
 export const logout = () => (dispatch, getState) => {
+  axios
+    .post('/api/auth/logout', null, tokenConfig(getState))
+    .then(res => {
+      dispatch({
+        type: LOGOUT_SUCCESS
+      })
+    }).catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status))
+    })
+}
+
+// Setup config with token - helper function
+export const tokenConfig = getState => {
   // Get token from state
   const token = getState().auth.token
 
@@ -89,13 +87,5 @@ export const logout = () => (dispatch, getState) => {
     config.headers['Authorization'] = `Token ${token}`
   }
 
-  axios
-    .post('/api/auth/logout', null, config)
-    .then(res => {
-      dispatch({
-        type: LOGOUT_SUCCESS
-      })
-    }).catch(err => {
-      dispatch(returnErrors(err.response.data, err.response.status))
-    })
+  return config
 }
